fa15250cd8b8777c413cc1b14b64d0b974653dfc
nehap910 <pokharkarneha2017@gmail.com>
2024-07-12 14:24:17 +0530

    banner page is ready

--- frontend/frontend_bookstore/src/components/Navbar.jsx
+++ frontend/frontend_bookstore/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
 
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-10 px-4">
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div className="navbar bg-base-100">
           <div className="navbar-start">
             <div className="dropdown">
