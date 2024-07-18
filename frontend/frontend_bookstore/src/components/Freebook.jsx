27b9168104e198f66d69065692f31b7077005b14
nehap910 <pokharkarneha2017@gmail.com>
2024-07-18 18:26:01 +0530

    data fetched from backend api

--- frontend/frontend_bookstore/src/components/Freebook.jsx
+++ frontend/frontend_bookstore/src/components/Freebook.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-import list from "../../public/list.json";
+import React, { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Cards from "./Cards";
+import axios from "axios";
 
 function Freebook() {
-  //to filter the data whose category is Free
-  const filterData = list.filter((data) => data.category === "free");
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data.filter((data) => data.category === "free"));
+        setBook(res.data.filter((data) => data.category === "free"));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
 
   var settings = {
     dots: true,
@@ -29,7 +41,7 @@ function Freebook() {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow:1 ,
+          slidesToShow: 1,
           slidesToScroll: 1,
           initialSlide: 1,
         },
@@ -58,7 +70,7 @@ function Freebook() {
 
         <div>
           <Slider {...settings}>
-            {filterData.map((item) => (
+            {book.map((item) => (
               <Cards item={item} key={item.id} />
             ))}
           </Slider>
